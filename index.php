351744209bbdea9edf83db56a0e63de3f3cfb81b
root <root@oselab-e2e3.oslab.opentlc.com>
2017-04-27 23:55:52 -0400

    First commit

--- index.php
+++ index.php
@@ -3,7 +3,9 @@ echo "<h1>Hello OpenShift!<h1>";
 echo "Greetings:";
 echo getenv("GREETINGS");
 echo "<br>";
-
+echo "<h1>Hello OpenShift! CI test<h1>";
+echo "Greetings:";
+echo getenv("GREETINGS");
 echo "Container Name: ";
 echo gethostname();
 echo "<br>";
